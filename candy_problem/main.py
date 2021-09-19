2da2cb9e93e3b38bc496d1c6b4d2857e1586bd39
Aisha <ishaarigbabu@gmail.com>
2021-09-19 12:20:10 -0500

    three functions written and tests to test funcions

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -25,12 +25,51 @@ the data structure made in `create_new_candy_data_structure()`.
 
 
 def create_new_candy_data_structure(data):
-    pass 
+    """
+    Function takes a list of lists. list object always
+    contains names at postion 0 and list of candy preferences
+    in index 1. function returns ditionary of names as keys and
+    values as list of candy preferences
+    """
+    # friends_candy_preferences = []
 
+
+    # for i in data:
+    #     dict_of_friend = {}
+    #     dict_of_friend["name"] = i[0]
+    #     dict_of_friend["candy preferences"] = i[1]
+
+    # friends_candy_preferences.append(dict_of_friend)
+
+    # return friends_candy_preferences
+
+    friends_candy_preferences_dict = {}
+
+    for i in data:
+        friends_candy_preferences_dict[i[0]] = i[1]
+
+    return friends_candy_preferences_dict
 
 def get_friends_who_like_specific_candy(data, candy_name):
-    pass
+    """
+    function takes data, dictionary,keys are names and values are candy preferences
+    returns list of names that like candy_name. takes two args dict and candyname
+    """
+    list_of_friends_that_like_candyname = []
 
+    for friend in data:
+        if candy_name in data[friend]:
+            list_of_friends_that_like_candyname.append(friend) 
+
+    return list_of_friends_that_like_candyname
 
 def create_candy_set(data):
-    pass 
+    
+    combined_list = []
+
+    for key,value in data.items():
+        combined_list += value
+
+    candy_set = set(combined_list)
+
+    return candy_set
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -16,7 +16,23 @@ def test_create_candy_data_structure_type():
     
     # Assert
     assert type(new_candy_data) == dict
-    
+
+def test_create_new_candy_data_structure_length():
+    #Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    #Act
+    new_candy_data = create_new_candy_data_structure(friend_favorites)
+
+    #Assert
+    assert len(new_candy_data) == len(friend_favorites)
+
+
 
 def test_create_candy_data_structure_values():
 
@@ -32,4 +48,37 @@ def test_create_candy_data_structure_values():
     new_candy_data = create_new_candy_data_structure(friend_favorites)
     
     # Assert
-    
+    assert type(new_candy_data.values) == list
+
+def test_get_friends_who_like_specific_candy_returns_list_of_friends_that_like_lollipop():
+
+    #Arrange
+    friends_favorites_dict = {
+        'Sally': ['lollipop', 'bubble gum', 'laffy taffy'],
+        'Bob': ['milky way', 'licorice', 'lollipop'],
+        'Arlene': ['chocolate bar', 'milky way', 'laffy taffy'],
+        'Carlie': ['nerds', 'sour patch kids', 'laffy taffy']
+    }
+
+    #Act
+    list_of_friends_that_like_lollipop = get_friends_who_like_specific_candy(friends_favorites_dict, "lollipop")
+
+    #Assert
+    assert "Sally" in list_of_friends_that_like_lollipop
+    assert "Bob" in list_of_friends_that_like_lollipop
+    assert len(list_of_friends_that_like_lollipop) == 2
+def test_create_candy_set_type():
+
+    # Arrange
+    friends_favorites_dict = {
+        'Sally': ['lollipop', 'bubble gum', 'laffy taffy'],
+        'Bob': ['milky way', 'licorice', 'lollipop'],
+        'Arlene': ['chocolate bar', 'milky way', 'laffy taffy'],
+        'Carlie': ['nerds', 'sour patch kids', 'laffy taffy']
+    }
+
+    #Act
+    new_candy_data = create_candy_set(friends_favorites_dict)
+
+    #Assert
+    assert type(new_candy_data) == set
